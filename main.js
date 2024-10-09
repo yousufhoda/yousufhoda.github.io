e3295b1dadf46f36dcf3f09d3c8282298578523e
yousufhoda <yousuf.hoda@torontomu.ca>
2024-10-09 18:34:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // a. Button
 
-// This function will prompt the user for their name and display it on the webpage
+// This function asks the user to enter their name using a button, once they've entered their name a personalized message will apprear
 function askName() {
 
     var userName = prompt("Please enter your name:");
@@ -8,23 +8,24 @@ function askName() {
         document.getElementById("nameOutput").innerHTML = "Hello, " + userName + "!";
     }
 }
-// Assign the function to the button's onclick event
+// Makes the button clickable
 document.getElementById("nameBtn").onclick = askName;
 
 // b.Clickable Image
 function enlargeImage(){
-    var img = document.getElementById("profileImg");
-    img.style.width ="400px";
+    var img = document.getElementById("profileImg"); // Get the image element bu its ID
+    img.style.width ="400px"; //The width will be increased to 400px once clicked
 }
 
+//Here we are assigning the enlargeImage function to the image with ID "profileImg"
 document.getElementById("profileImg").onclick = enlargeImage;
 
 //c. Navigation Menu
 
-// Function to toggle the dropdown when clicking on the 'Degree' menu item
+// Function to ensure dropdown menu appears when clicked and disappears when clicked outside of it
 document.addEventListener("DOMContentLoaded", function() {
-    var dropdownToggle = document.querySelector('.dropdown-toggle'); // Target the degree link
-    var dropdownMenu = document.querySelector('.dropdown'); // Target the dropdown menu
+    var dropdownToggle = document.querySelector('.dropdown-toggle'); // get the degree link
+    var dropdownMenu = document.querySelector('.dropdown'); // Get the dropdown menu
     
     // Function to toggle the dropdown menu visibility
     function toggleDropdown() {
@@ -35,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
     window.onclick = function(event) {
         if (!event.target.matches('.dropdown-toggle')) {
             if (dropdownMenu.classList.contains('show')) {
-                dropdownMenu.classList.remove('show');
+                dropdownMenu.classList.remove('show'); //Remove the 'show' class to hide the menu
             }
         }
     };
